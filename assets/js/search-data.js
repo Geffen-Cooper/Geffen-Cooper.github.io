f9820558e9297c2934ac495f3fbb4dfe87595a9a
Geffen-Cooper <geffencooper@gmail.com>
2025-03-09 05:14:41 +0000

    Deploying to gh-pages from @ Geffen-Cooper/geffen-cooper.github.io@2642da45a4ac6ccf11e1f333966c8315c69fd150 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -387,16 +387,14 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-i-start-my-ph-d-at-ut-austin",
+          title: 'I start my Ph.D. at UT Austin!',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+          section: "News",},{id: "news-iotdi-paper-and-two-demos-accepted-we-received-a-professional-development-award-to-present-this-work-in-hong-kong",
+          title: 'IoTDI Paper and two demos accepted! We received a professional development award to...',
           description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          section: "News",},{id: "news-ieee-bsn-paper-accepted-i-also-received-a-travel-award-to-attend-the-conference-in-chicago",
+          title: 'IEEE BSN Paper accepted! I also received a travel award to attend the...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
